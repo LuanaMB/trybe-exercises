d4c2fd0d86a29ce2c5b690e86a678752b454775a
LuanaMB <luanabragaaa@hotmail.com>
2021-06-24 15:37:31 -0300

    exercicio ate o numero 2

---
+++ bloco_4/arrayEloopFor/index.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let index=0;index < numbers.length; index +=1 ){
+  console.log(numbers[index]);
+}
+//2)
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma=0;
+for(let i=0;i<numbers.length;i+=1){
+  soma += numbers[i]
+}
+console.log(soma)
+//3
