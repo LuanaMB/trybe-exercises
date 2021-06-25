527c74c8989859367b5fa847a7ff1b518c71d3fe
LuanaMB <luanabragaaa@hotmail.com>
2021-06-25 15:54:36 -0300

    até exercicio numero 3

---
+++ bloco_4/logica-de-programacao-e-algoritmos/index.js
@@ -0,0 +1,21 @@
+//1
+let n =5;
+let simbolo= '*';
+for(let index2=1; index2<=n; index2 +=1){
+let quadrado='';
+  for(let index=1; index<=n; index += 1){
+    quadrado+=simbolo      
+  }
+  console.log(quadrado)
+}
+//2
+let n =5;
+let quadrado='';
+let simbolo= '*';
+for(let index=1; index<=n; index += 1){
+  quadrado+=simbolo    
+  console.log(quadrado)
+}
+//3
+
+
