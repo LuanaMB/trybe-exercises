9de0cfc3e3e85f38ce4495dd0d4135785988556f
LuanaMB <luanabragaaa@hotmail.com>
2021-06-28 15:49:59 -0300

    exemplos for/in

--- bloco_4/Objetos-Funções/index.js
+++ bloco_4/Objetos-Funções/index.js
@@ -12,3 +12,22 @@ let player= {
 console.log('A jogadora ' + player.nome + ' foi eleita a melhor do mundo por '  + player.bestInTheWorld.length + ' vezes');
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+
+let nomes = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' ,
+};
+for(let index in nomes){
+  console.log('Olá ' + nomes[index]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+for(let index in car){
+  console.log(index, car[index]);
+}
+
