7e0a761e8e063e992e5c1b86febd091f5f7e067a
LuanaMB <luanabragaaa@hotmail.com>
2021-07-25 15:21:19 -0300

    exercicios conteudo

---
+++ FundamentosWeb/bloco_7/7.2/script.js
@@ -0,0 +1,65 @@
+const student1 = {
+  Html: "Muito Bom",
+  Css: "Bom",
+  JavaScript: "Ótimo",
+  SoftSkills: "Ótimo",
+};
+
+const student2 = {
+  Html: "Bom",
+  Css: "Ótimo",
+  JavaScript: "Ruim",
+  SoftSkills: "Ótimo",
+  Git: "Bom", // chave adicionada
+};
+
+const objetcs = (objeto) => {
+  let obj = Object.keys(objeto);
+  let value = Object.values(objeto)
+  for (index in obj) {
+    console.log(`${obj[index]}, Nivel: ${value[index]}`);
+  }
+};
+
+objetcs(student1);
+
+//parte values
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsValuesWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+
+// Sem Object.values
+console.log(listSkillsValuesWithFor(student));
+
+// Com Object.values
+console.log(listSkillsValuesWithValues(student));
+
+//parte entries
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
