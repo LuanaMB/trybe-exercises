0e2198c743597b3a89be9220dbfa74cd158f6d02
LuanaMB <luanabragaaa@hotmail.com>
2021-07-28 19:36:35 -0300

    parte do exercicios bonus

---
+++ FundamentosWeb/bloco_8/ex.praticos/exercicio3.js
@@ -0,0 +1,24 @@
+const RIGHT_ANSWERS = ["A", "C", "B", "D", "A", "A", "D", "A", "D", "C"];
+const STUDENT_ANSWERS = ["A", "N.A", "B", "D", "A", "C", "N.A", "A", "D", "B"];
+
+const repeat = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionLet = action(rightAnswers[index], studentAnswers[index])
+    contador += actionLet
+  }
+  return `Resultado final: ${contador} corretas`;
+};
+
+const condicao = (parametro1, parametro2) => {
+  if (parametro1 === parametro2) {
+    return 1;
+  } else if (parametro1 === "N.A") {
+    return 0;
+  }
+  return -0.5;
+};
+
+console.log(repeat(RIGHT_ANSWERS, STUDENT_ANSWERS, condicao))
+
+
